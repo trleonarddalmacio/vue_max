de80a2e721ecf4b278233d1e47e7ccf37e80c0b0
Leonard Dalmacio <leonarddalmacio@Thrush.local>
2018-05-01 11:48:27 +0800

    Updated Lesson 9

--- 09 Advance Component Usage/src/App.vue
+++ 09 Advance Component Usage/src/App.vue
@@ -2,7 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+               <app-quote>
+                   <h2 slot="title"> {{ quoteTitle }} </h2>
+                   <p>A wonderful Quote</p>
+               </app-quote>
             </div>
         </div>
     </div>
@@ -10,9 +13,20 @@
 
 <script>
 
+    import Quote from './components/Quote.vue'
+
     export default {
-        
+        data: function () {
+            return {
+                quoteTitle: 'The Quote'
+            }
+        },
+
+        components: {
+            appQuote: Quote
+        }
     }
+
 </script>
 
 <style>
