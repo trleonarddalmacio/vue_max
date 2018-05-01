271ca066262d6d0b102733de97e07c2d161fd48f
Leonard Dalmacio <leonarddalmacio@Thrush.local>
2018-05-01 17:26:09 +0800

    Finished 9th Lesson and Starting the 10th Lesson, Wonderful Quotes

--- 09 Advance Component Usage/src/App.vue
+++ 09 Advance Component Usage/src/App.vue
@@ -2,29 +2,46 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-quote>
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p> {{ selectedComponent  }} </p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>
+                
+                <!-- <app-quote>
                    <h2 slot="title"> {{ quoteTitle }} </h2>
                    <p>A wonderful Quote</p>
-               </app-quote>
+                </app-quote> -->
             </div>
-        </div>
+        </div> 
     </div>
 </template>
 
 <script>
 
-    import Quote from './components/Quote.vue'
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
         data: function () {
             return {
-                quoteTitle: 'The Quote'
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote',
             }
         },
 
         components: {
-            appQuote: Quote
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
         }
+
     }
 
 </script>
--- 09 Advance Component Usage/src/components/Author.vue
+++ 09 Advance Component Usage/src/components/Author.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="container">
-    
+    <div>
+        <h3>The Author</h3>
     </div>
 </template>
 
 <script>
     export default {
-        
+
     }
+
 </script>
 
-<style>
+<style scoped>
 </style>
